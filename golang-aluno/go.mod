f6395b454aaa7dc83952cea8fa12ddcf5d741648
matheusapostulo <matheusapostulo10@gmail.com>
2024-07-04 00:54:59 -0300

    Aplicação base em GO

---
+++ golang-aluno/go.mod
@@ -0,0 +1,5 @@
+module github.com/devfullcycle/imersao18/golang
+
+go 1.22.5
+
+require github.com/google/uuid v1.6.0
---
+++ golang-aluno/internal/events/domain/spot.go
@@ -0,0 +1,73 @@
+package domain
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidSpotNumber        = errors.New("invalid spot number")
+	ErrSpotNotFound             = errors.New("spot not found")
+	ErrSpotAlreadyReserved      = errors.New("spot already reserved")
+	ErrSpotNameTwoCharacters    = errors.New("spot name must have at least two characters")
+	ErrSpotNameRequired         = errors.New("spot name is required")
+	ErrSpotNameStartWithLetter  = errors.New("spot name must start with a letter")
+	ErrSpotNameFinishWithNumber = errors.New("spot name must end with a number")
+)
+
+type SpotStatus string
+
+const (
+	SpotStatusAvailable SpotStatus = "available"
+	SpotStatusSold      SpotStatus = "sold"
+)
+
+type Spot struct {
+	ID       string
+	EventID  string
+	Name     string
+	Status   SpotStatus
+	TicketID string
+}
+
+func NewSpot(event Event, name string) (*Spot, error) {
+	spot := &Spot{
+		ID:      uuid.New().String(),
+		EventID: event.ID,
+		Name:    name,
+		Status:  SpotStatusAvailable,
+	}
+
+	if err := spot.Validate(); err != nil {
+		return nil, err
+	}
+	return spot, nil
+}
+
+// Validate checks if the spot data is valid.
+func (s *Spot) Validate() error {
+	if len(s.Name) == 0 {
+		return ErrSpotNameRequired
+	}
+	if len(s.Name) < 2 {
+		return ErrSpotNameTwoCharacters
+	}
+	// Validate if the spot name is in the correct format
+	if s.Name[0] < 'A' || s.Name[0] > 'Z' {
+		return ErrSpotNameStartWithLetter
+	}
+	if s.Name[1] < '0' || s.Name[1] > '9' {
+		return ErrSpotNameFinishWithNumber
+	}
+	return nil
+}
+
+func (s *Spot) Reserve(ticketID string) error {
+	if s.Status == SpotStatusSold {
+		return ErrSpotAlreadyReserved
+	}
+	s.Status = SpotStatusSold
+	s.TicketID = ticketID
+	return nil
+}
